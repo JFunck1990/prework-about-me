e13829a5e35cb97694181eadc04a6752d48eb83d
JFunck1990 <59205926+JFunck1990@users.noreply.github.com>
2020-11-16 15:18:51 -0500

    Add files via upload

---
+++ Module-7 Scratch Activity/Scratch.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext('2d')
+
+ctx.fillStyle = '#f00000'
+ctx.fillRect(20, 20, 20, 20);
+
+ctx.fillStyle = '#00000F';
+ctx.fillRect(20, 20, 50, 50);
+
+
